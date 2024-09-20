06cc568083d8d41d31cf4f032ea98be191fab3f1
Jack Darlington <jackdarlington2001@gmail.com>
2024-09-20 17:29:32 +1200

    Initial backend setup to run on port 5000. MongoDB uri in .env as MONGODB_URI

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+        dbName: 'speed',
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 5000;
+
+  app.enableCors();
+
+  await app.listen(port);
+
+  Logger.log(`Application is running on http://localhost:${port}`, 'Speed');
 }
 bootstrap();
