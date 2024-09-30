07c55350cd930827b1c40b97d52733d32567c991
Jack Darlington <jackdarlington2001@gmail.com>
2024-09-30 14:11:43 +1300

    Fix dist add in gitignore

--- backend/dist/article/article.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { ArticleService } from './article.service';
-export declare class ArticleController {
-    private readonly articleService;
-    constructor(articleService: ArticleService);
-    getArticles(): Promise<import("./article.schema").Article[]>;
-    getArticleById(id: string): Promise<import("./article.schema").Article>;
-}
